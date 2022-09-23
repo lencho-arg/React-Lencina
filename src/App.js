905a9ceb59c810b828cc76149c08d00592221bd2
Lencina Federico <lencina_federicon@hotmail.com>
2022-09-22 21:24:06 -0300

    React

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <NavBar />
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
